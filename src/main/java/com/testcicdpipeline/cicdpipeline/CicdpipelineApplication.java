9efc6a8cb10d5edb6a320dfc61bd92d4e3e9c37f
Bala <mailtobalas@gmail.com>
2023-05-23 13:25:42 +0530

    second commit

---
+++ src/main/java/com/testcicdpipeline/cicdpipeline/CicdpipelineApplication.java
@@ -0,0 +1,15 @@
+package com.testcicdpipeline.cicdpipeline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicdpipelineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CicdpipelineApplication.class, args);
+
+		System.out.println("test...");
+	}
+
+}
