378172af6bdb3d3aac21c7beefe54940c426924c
Bala <mailtobalas@gmail.com>
2023-05-24 12:08:06 +0530

    commit msg from intellij

--- src/main/java/com/testcicdpipeline/cicdpipeline/CicdpipelineApplication.java
+++ src/main/java/com/testcicdpipeline/cicdpipeline/CicdpipelineApplication.java
@@ -9,7 +9,7 @@ public class CicdpipelineApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CicdpipelineApplication.class, args);
 
-		System.out.println("test testing...");
+		System.out.println("test testing1...");
 	}
 
 }
